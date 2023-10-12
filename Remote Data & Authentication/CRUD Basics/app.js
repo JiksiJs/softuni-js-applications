3c74475e04a1c2b7f0f628c0b5dcd64a3480b6e1
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-10-12 17:37:57 +0100

    Updated version
    Error handling added

--- Remote Data & Authentication/CRUD Basics/app.js
+++ Remote Data & Authentication/CRUD Basics/app.js
@@ -2,46 +2,67 @@ start();
 
 function start() {
   document.getElementById("editor_create").addEventListener("submit", postData);
-  document.getElementById("save_btn").addEventListener("click", savePart);
   document.getElementById("load_btn").addEventListener("click", loadData);
   document.getElementById("table_body").addEventListener("click", tableAction);
-  document
-    .getElementById("cancel_btn")
-    .addEventListener("click", toggleEditors);
+  document.getElementById("save_btn").addEventListener("click", savePart);
 }
 
 async function postData(event) {
   event.preventDefault();
+
+  //take input values
   const formData = new FormData(event.target);
+  const { label, price, qty } = Object.fromEntries(formData.entries());
+
+  //error handling
+  if (label == "" || price == "" || qty == "") {
+    return alert("All fields are required!");
+  }
+
+  //post request to the server
   const url = "http://localhost:3030/jsonstore/autoparts";
 
-  const partData = {
-    label: formData.get("label"),
-    price: Number(formData.get("price")),
-    qty: Number(formData.get("qty")),
+  const postData = {
+    label,
+    price,
+    qty,
   };
-
   const options = {
     method: "post",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(partData),
+    body: JSON.stringify(postData),
   };
 
-  const response = await fetch(url, options);
-  const result = await response.json();
-  console.log(result);
-  loadData();
+  try {
+    const response = await fetch(url, options);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    const data = await response.json();
+    // loadData();
+  } catch (error) {
+    alert(error.message);
+  }
   event.target.reset();
 }
 
 async function loadData() {
   const url = "http://localhost:3030/jsonstore/autoparts";
 
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    const response = await fetch(url);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    const data = await response.json();
 
-  const row = Object.values(data).map(createRow);
-  document.getElementById("table_body").replaceChildren(...row);
+    const row = Object.values(data).map(createRow);
+    document.getElementById("table_body").replaceChildren(...row);
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 function createRow(record) {
@@ -61,7 +82,7 @@ function createRow(record) {
 function tableAction(event) {
   const target = event.target;
 
-  if (target.tagName === "BUTTON") {
+  if (target.tagName == "BUTTON") {
     if (target.classList.contains("delete_btn")) {
       deleteRecord(target.dataset.id);
     } else if (target.classList.contains("edit_btn")) {
@@ -79,25 +100,39 @@ async function deleteRecord(recordId) {
   const options = {
     method: "delete",
   };
-
-  const response = await fetch(url, options);
+  try {
+    const response = await fetch(url, options);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+  } catch (error) {
+    alert(error.message);
+  }
   loadData();
 }
 
 async function loadForEditing(recordId) {
   const url = "http://localhost:3030/jsonstore/autoparts/" + recordId;
 
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(url);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    const data = await response.json();
 
-  document.getElementById("editor_create").style.display = "none";
-  document.getElementById("editor_edit").style.display = "block";
+    document.getElementById("editor_create").style.display = "none";
+    document.getElementById("editor_edit").style.display = "block";
 
-  document.getElementById("edit_part_id").value = data._id;
-  document.getElementById("edit_part_label").value = data.label;
-  document.getElementById("edit_part_price").value = data.price;
-  document.getElementById("edit_part_qty").value = data.qty;
+    document.getElementById("edit_part_id").value = data._id;
+    document.getElementById("edit_part_label").value = data.label;
+    document.getElementById("edit_part_price").value = data.price;
+    document.getElementById("edit_part_qty").value = data.qty;
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 async function savePart() {
@@ -115,8 +150,16 @@ async function savePart() {
     body: JSON.stringify(record),
   };
 
-  const response = await fetch(url, options);
-  const result = await response.json();
+  try {
+    const response = await fetch(url, options);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    const result = await response.json();
+  } catch (error) {
+    alert(error.message);
+  }
   loadData();
   toggleEditors();
 }
