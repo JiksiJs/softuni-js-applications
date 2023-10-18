0b8277f59cbff0f4a52f279eb7cd8323d1e051ff
Zhivko Shopov <103967363+JiksiJs@users.noreply.github.com>
2023-10-18 14:56:53 +0100

    Add files via upload
    Solved 2nd problem "Phonebook"

--- Remote Data & Authentication/Exercises/02.Phonebook/app.js
+++ Remote Data & Authentication/Exercises/02.Phonebook/app.js
@@ -1,5 +1,123 @@
 function attachEvents() {
-    console.log('TODO...');
+  document.getElementById("btnCreate").addEventListener("click", postData);
+  document.getElementById("btnLoad").addEventListener("click", loadData);
 }
 
 attachEvents();
+
+async function postData() {
+  const name = document.getElementById("person");
+  const phone = document.getElementById("phone");
+  const url = "http://localhost:3030/jsonstore/phonebook";
+
+  if (name.value == "" || phone.value == "") {
+    return alert("All fields are required!");
+  }
+
+  const userData = {
+    person: name.value,
+    phone: phone.value,
+  };
+  const options = {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    loadData();
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+async function loadData() {
+  const url = "http://localhost:3030/jsonstore/phonebook";
+
+  try {
+    const response = await fetch(url);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+    const data = await response.json();
+    console.log(data);
+
+    Object.keys(data).forEach((key) => localStorage.setItem("key", key));
+    Object.values(data).map(createListItem);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function createListItem(record) {
+  const element = document.createElement("li");
+  const list = document.getElementById("phonebook");
+
+  element.textContent = `${record.person}: ${record.phone}`;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete_btn");
+  deleteButton.textContent = "Delete";
+  element.appendChild(deleteButton);
+
+  deleteButton.addEventListener("click", async (event) => {
+    const key = localStorage.getItem("key");
+    const url = `http://localhost:3030/jsonstore/phonebook/${key}`;
+
+    const options = {
+      method: "delete",
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (response.ok == false) {
+        const error = await response.json();
+        throw error;
+      }
+      event.target.parentNode.remove();
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+
+  list.appendChild(element);
+
+  return list;
+}
+
+/*
+async function deleteRequest() {
+  const key = localStorage.getItem("key");
+  console.log(key);
+  const url = `http://localhost:3030/jsonstore/phonebook/${key}`;
+
+  const options = {
+    method: "delete",
+  };
+  try {
+    const response = await fetch(url, options);
+    if (response.ok == false) {
+      const error = await response.json();
+      throw error;
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+function deleteRecord(event) {
+  const target = event.target;
+
+  if (target.tagName === "BUTTON") {
+    if (target.classList.contains("delete_btn")) {
+      console.log("clicked delete");
+    }
+  }
+}
+*/
